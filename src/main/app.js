f86c95fdc67767da66b253f053f5f06f6ff0cd3f
mirkog-navent <mpinas@navent.com>
2024-05-23 16:46:18 -0300

    clear project

--- src/main/app.js
+++ src/main/app.js
@@ -12,8 +12,8 @@ app.set('port', process.env.PORT || 8080)
 app.use(cors())
 app.use(express.json());
 
-app.post('/coupon', limiter, require('./routes/coupon.js'))
-app.get('/coupon', limiter, require('./routes/coupon.js'))
+app.post('/coupon', require('./routes/coupon.js'))
+app.get('/coupon', require('./routes/coupon.js'))
 app.get('/', (req, res) => {res.send('Coupon API')});
 
 module.exports = app;
