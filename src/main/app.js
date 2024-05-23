f15e680899ce4b424c34ec56688c7e1ede9d680e
mirkog-navent <mpinas@navent.com>
2024-05-23 16:26:06 -0300

    clear project

--- src/main/app.js
+++ src/main/app.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const compression = require('compression');
-const rateLimit = require('express-rate-limit');
 
 const app = express();
 
 app.use(compression());
-
-const limiter = rateLimit({
-    windowMs: 1 * 60 * 1000, 
-    max: 100000,
-    message: "Too many requests, please try again later."
-  });
-
-app.use(limiter);
   
 
 app.set('port', process.env.PORT || 8080)
